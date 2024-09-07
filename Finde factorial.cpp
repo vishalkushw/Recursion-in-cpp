599db10ab2b27b5c8f147fcf9705cf137d4a7837
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-07 21:18:42 +0530

    Finde factorial

---
+++ Finde factorial.cpp
@@ -0,0 +1,18 @@
+// finde factorial
+#include <iostream>
+using  namespace std;
+
+int fact(int n){
+	if(n==0){
+		return 1;
+	}
+	
+	int prevfact=fact(n-1);
+	return n*prevfact;
+}
+int main(){
+	int n;
+	cout<<"enter number : ";
+	cin>>n;
+	cout<<fact(n);
+}
